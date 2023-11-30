c2605593d28fafa0803497b5a889a54850e3902b
Dmitriy Trubnikov <truq1990@gmail.com>
2023-11-30 23:37:20 +0400

    Update .gitignore

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,13 +1,22 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
     @Autowired
+    @Qualifier ("dog")
     private Animal animal;
+
+    @Autowired
+    private Timer time;
+
+    public Timer getTimer() {
+        return time;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
